dbbd902fabc8011a40d5ca4af53d5e4e2d534f5e
JenMora <jeniffermourah73@gmail.com>
2023-08-27 12:06:00 -0700

    This is a file that conatins the bubble sort algorithm that sorts  data by bubbling the larger data to teh right

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+* bubble_sort - Sorts an array of integers
+* in ascending order using Bubble sort.
+* @array: The array to be sorted.
+* @size: The size of the array.
+*/
+void bubble_sort(int *array, size_t size)
+{
+int swapped;
+size_t i = 0;
+size_t j = 0;
+
+
+for (i = 0; i < size - 1; i++)
+{
+swapped = 0;
+
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+int temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+swapped = 1;
+
+print_array(array, size);
+}
+}
+
+if (!swapped)
+break;
+}
+}
